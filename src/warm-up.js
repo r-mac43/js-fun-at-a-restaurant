216cdf78bac9b22c36d61db219dfded2ae5bbaa7
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-10-12 19:31:37 -0700

    Complete warm up exercies

--- src/warm-up.js
+++ src/warm-up.js
@@ -101,8 +101,15 @@ fruits[4];
 // Create a new variable called "groceryStore" and assign to it an object literal with two
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
-
+var groceryStore = {
+    name: "Safeway",
+    isOpen: true,
+    hours: "9am to 9pm",
+    hasOrganic: true,
+    managerName: "Steve",
+}
 
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.location = "Denver";
