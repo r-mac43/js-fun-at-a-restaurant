59c0fa5c07220ba19e3be638d185bec296254ccb
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-10-12 19:19:21 -0700

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -12,26 +12,36 @@
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
-
+var food = 'pizza';
+var type = 'entree';
+var inStock = 5
 
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
+inStock = 5 * 2;
 
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock = 5 * 2 -1;
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = true;
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+var shouldOrderMore;
+if (inStock < 20) {
+    shouldOrderMore = true;
+} else {
+    shouldOrderMore = false;
+};
+
 
 
 
@@ -42,14 +52,30 @@
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
+var stockCount = 5;
+function countStock(){
+    if (stockCount >= 1) {
+        console.log(`I'm going to eat some ${food}!`);
 
+    } else {
+        console.log(`We need to get some more ${food},`);
+    }
+
+}
 
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
+function findFavorite() {
+    if (isFavorite = true) {
+        console.log(`My favorite food is ${food}!`);
 
+    } else {
+        console.log(`I recently ate ${food}.`);
+    }
+}
 
 
 //------------------------- Arrays -------------------------//
@@ -57,18 +83,18 @@
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+var vegetables = ['carrots', 'lettuce', 'radishes', 'cucumbers', 'spinach'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+vegetables[2];
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
-
+var fruits = ['strawberries', 'blueberries', 'apples', 'grapes', 'bananas', 'oranges', 'cherries'];
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+fruits[4];
 
 //------------------------- Objects -------------------------//
 
